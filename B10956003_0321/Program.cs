127fa340d8879c6d72ffd7f80bfb056ef783f3fd
user <user@DESKTOP-6JBEF5K>
2022-03-21 11:55:36 +0800

    123

--- B10956003_0321/Program.cs
+++ B10956003_0321/Program.cs
@@ -1,25 +1,60 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace B10956003_0321
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
             //int result = Add(3, 5);
             //Console.WriteLine(result);
-            Car myCar = new Car();
-            myCar.ShowInfo();
+            //Car myCar = new Car();
+            //myCar.ShowInfo();
             //myCar.ChangeColor("blue");
             //myCar.ShowInfo();
 
-            Car CarA = new CarA();
-            CarA.OpenDoor();
+            //Car CarA = new CarA();
+            //CarA.OpenDoor();
+
+            //await MyHttpClientAsync();
+
+
+        static private async Task MyHttpClientAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                string url = "http://www.contoso.com/";
+
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(url);
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
         }
-        static private int Add(int a,int b)
+        static private int Add(int a, int b)
         {
             return a + b;
+
+        }
+        static private int MyRandom(int minNum, int maxNum)
+        {
+            Random random = new Random();
+            return random.Next(minNum, maxNum + 1);
+
         }
     }
 }
